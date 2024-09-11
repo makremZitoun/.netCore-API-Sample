cb147151015b214e379d486093a4b37de5ceffff
makrem zitoun <makremzitoun242@gmail.com>
2024-09-11 20:51:45 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_B4ps_v2"
   }
 
